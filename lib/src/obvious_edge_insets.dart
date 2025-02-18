781dbf9a691a942d09b73e989cb7ef839f9aa871
Rafael <rafaelcolladojr@gmail.com>
2025-02-18 12:24:05 -0500

    fix: Replace constructor param limit assert with ArgumentError throw

--- lib/src/obvious_edge_insets.dart
+++ lib/src/obvious_edge_insets.dart
@@ -21,23 +21,35 @@ class ObviousEdgeInsets extends EdgeInsets {
   ///
   /// {@end-tool}
   /// If more than one is provided, an [AssertionError] is thrown.
-  const ObviousEdgeInsets.except({
+  ObviousEdgeInsets.except({
     double? left,
     double? top,
     double? right,
     double? bottom,
-  })  : assert(
-          (left == null ? 0 : 1) +
-                  (top == null ? 0 : 1) +
-                  (right == null ? 0 : 1) +
-                  (bottom == null ? 0 : 1) ==
-              1,
-          'ObviousEdgeInsets.except must only accept one argument.',
-        ),
+  })  : assert(_validateParams(left, top, right, bottom), ''),
         super.fromLTRB(
           top ?? right ?? bottom ?? 0, // left
           left ?? right ?? bottom ?? 0, // top
           left ?? top ?? bottom ?? 0, // right
           left ?? top ?? right ?? 0, // bottom
         );
+
+  static bool _validateParams(
+    double? left,
+    double? top,
+    double? bottom,
+    double? right,
+  ) {
+    final valid = (left == null ? 0 : 1) +
+            (top == null ? 0 : 1) +
+            (right == null ? 0 : 1) +
+            (bottom == null ? 0 : 1) ==
+        1;
+    if (!valid) {
+      throw ArgumentError(
+        'ObviousEdgeInsets.except must only accept one argument.',
+      );
+    }
+    return valid;
+  }
 }
