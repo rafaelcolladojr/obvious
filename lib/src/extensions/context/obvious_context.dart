489ef656a43d2a2f3e5f606f8232683e5a9e59ba
Rafael <rafaelcolladojr@gmail.com>
2025-03-07 16:48:09 -0500

    fix: Added BuildContext extensions to export list

---
+++ lib/src/extensions/context/obvious_context.dart
@@ -0,0 +1,4 @@
+export 'obvious_context_dialog.dart';
+export 'obvious_context_media_query.dart';
+export 'obvious_context_navigator.dart';
+export 'obvious_context_theme.dart';
--- lib/src/extensions/obvious_extensions.dart
+++ lib/src/extensions/obvious_extensions.dart
@@ -1,1 +1,2 @@
+export 'context/obvious_context.dart';
 export 'num/obvious_num.dart';
