7d6601d628459aeb796ea779501938b2f71fe95d
Rafael <rafaelcolladojr@gmail.com>
2025-03-13 14:01:02 -0400

    docs: Cleaned String.nullIfEmpty documentation

--- lib/src/extensions/string/obvious_string.dart
+++ lib/src/extensions/string/obvious_string.dart
@@ -3,20 +3,5 @@
 extension ObviousStringExtension on String {
   /// Returns *null* if this String's
   /// isEmpty method returns *true*
-  ///
-  /// {@tool snippet}
-  ///
-  /// example:
-  /// ```dart
-  /// 12.heightBox
-  /// ```
-  ///
-  /// is synonimous with:
-  ///
-  /// ```dart
-  /// SizedBox(height: 12)
-  /// ```
-  ///
-  /// {@end-tool}
   String? nullIfEmpty() => isEmpty ? null : this;
 }
